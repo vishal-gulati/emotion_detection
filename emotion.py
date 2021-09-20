a7f22ce4a058e22e13869134b76b00e3137330ea
Vishal Gulati <vishalgulati2k@gmail.com>
2021-09-20 12:33:31 +0530

    initial push

---
+++ emotion.py
@@ -0,0 +1,49 @@
+try:
+    import numpy as np
+    import argparse
+    import matplotlib.pyplot as plt
+    import cv2
+    import tensorflow
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Dense, Dropout, Flatten
+    from tensorflow.keras.layers import Conv2D
+    from tensorflow.keras.optimizers import Adam
+    from tensorflow.keras.layers import MaxPooling2D
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    import os
+    import PIL.Image
+    import pandas as pd 
+    from PIL import Image
+    from tqdm import tqdm
+    import os
+    import sys
+except Exception as e:
+    print(e)
+
+oldname = sys.argv[1]
+newname=sys.argv[1]+".jpg"
+os.rename(oldname,newname)
+
+model1 = Sequential()
+model1.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model1.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model1.add(MaxPooling2D(pool_size=(2, 2)))
+model1.add(Dropout(0.25))
+model1.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model1.add(MaxPooling2D(pool_size=(2, 2)))
+model1.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model1.add(MaxPooling2D(pool_size=(2, 2)))
+model1.add(Dropout(0.25))
+model1.add(Flatten())
+model1.add(Dense(1024, activation='relu'))
+model1.add(Dropout(0.5))
+model1.add(Dense(7, activation='softmax'))
+model1.load_weights('data/model.h5')
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+frame=cv2.imread(newname)
+facecasc = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+img = np.expand_dims(np.expand_dims(cv2.resize(gray, (48, 48)), -1), 0)
+prediction = model1.predict(img)
+maxindex = int(np.argmax(prediction))
+print(emotion_dict[maxindex])
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const { spawn } = require('child_process')
+const app = express()
+const path=require("path");
+const ejs=require("ejs");
+const multer = require('multer')
+
+const upload = multer({
+  dest: 'data',
+  limits: {
+    fileSize: 1000000,
+  },
+  
+  fileFilter(req, file, cb) {
+  
+  if (!file.originalname.match(/\.(png|jpg|jpeg)$/)){
+    cb(new Error('Please upload an image.'))
+  }
+    cb(undefined, true)
+  }
+})
+  
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+  if(req.file){
+
+    console.log("File name",req.file.filename)
+    const python = spawn('python', ['emotion.py',`data/`+req.file.filename])
+
+    python.stdout.on('data', function (data) {
+      console.log('Pipe data from python script ...STARTS')
+      console.log(data.toString())
+      console.log('Pipe data from python script ...ENDS')
+    })
+    python.stdout.on('close', function (code) {
+        console.log('Closed with code ',code)
+        res.render('pages/image',{data: "Mosaic.jpeg"});
+      })
+    
+
+    
+  }else{
+    res.status(400).json({msg:"Img Not Received"})
+  }
+})
+
+app.get('/check',(req,res)=>{
+  console.log("Check Route")
+  res.render('pages/image',{data: "Mosaic.jpeg"});
+})
+
+app.get('/',(req,res)=>{
+  console.log("Home Route")
+  res.render('pages/index');
+})
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`)
+})
